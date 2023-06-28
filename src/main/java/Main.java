09bdaf2a49fb2482ae8853051ac6cecf376b3488
alexsavkaev <123101083+alexsavkaev@users.noreply.github.com>
2023-06-29 02:17:53 +0300

    cleaned up code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,7 +8,6 @@ public class Main {
         String data = "";
         while (!Objects.equals(data, "q")) {
             System.out.println("Введите ФИО, Возраст, Пол: ");
-            System.out.println("Либо q для выхода, s для сортировки.");
             data = in.nextLine();
             if (!Objects.equals(data, "q") && !Objects.equals(data, "s")) {
                 String[] tmp = data.split(",");
@@ -16,19 +15,14 @@ public class Main {
                 String result = fio[2] + " " + fio[0].toUpperCase().charAt(0)
                         + "." + fio[1].toUpperCase().charAt(0)+"." + tmp[1] + " " + tmp[2];
                 people.add(result);
-                for (String person : people) {
-                    System.out.println(person);
-                }
             }
-            if(Objects.equals(data, "s"))
-                System.out.println("Сортировка...");
-                people.sort((o1, o2) -> Integer.parseInt(o1.split(" ")[2])-Integer.parseInt(o2.split(" ")[2]));
-            for (String person : people) {
-                System.out.println(person);
             }
-
+        people.sort(Comparator.comparingInt(o -> Integer.parseInt(o.split(" ")[2])));
+        for (String person : people) {
+            System.out.println(person);
+            }
         }
     }
-}
+
 
 
