759eaf1557331620b8dd3f821b98c667e6ecdb0c
alexsavkaev <123101083+alexsavkaev@users.noreply.github.com>
2023-06-29 18:16:20 +0300

    added Sort and Quit options

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,16 +8,21 @@ public class Main {
         String data = "";
         while (!Objects.equals(data, "q")) {
             System.out.println("Введите ФИО, Возраст, Пол: ");
+            System.out.println("q для выхода, s для сортировки");
             data = in.nextLine();
-            if (!Objects.equals(data, "q") && !Objects.equals(data, "s")) {
+            if (!Objects.equals(data, "q") && (!Objects.equals(data, "s"))) {
                 String[] tmp = data.split(",");
                 String[] fio = tmp[0].split(" ");
                 String result = fio[2] + " " + fio[0].toUpperCase().charAt(0)
                         + "." + fio[1].toUpperCase().charAt(0)+"." + tmp[1] + " " + tmp[2];
                 people.add(result);
             }
+            if(Objects.equals(data, "s")) {
+                System.out.println("Сортировка...");
+                people.sort(Comparator.comparingInt(o -> Integer.parseInt(o.split(" ")[2])));
             }
-        people.sort(Comparator.comparingInt(o -> Integer.parseInt(o.split(" ")[2])));
+            }
+
         for (String person : people) {
             System.out.println(person);
             }
